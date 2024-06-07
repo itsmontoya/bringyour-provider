cc08526f444d338df19533174d2832877dcaa5a4
Josh Montoya <josh@montoya.io>
2024-06-07 12:42:14 -0700

    initial_pull: implement status

---
+++ libs/provider/status.go
@@ -0,0 +1,34 @@
+package provider
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type Status struct {
+}
+
+func (s *Status) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	type WarpStatusResult struct {
+		Version       string `json:"version,omitempty"`
+		ConfigVersion string `json:"config_version,omitempty"`
+		Status        string `json:"status"`
+		ClientAddress string `json:"client_address,omitempty"`
+		Host          string `json:"host"`
+	}
+
+	result := &WarpStatusResult{
+		Version:       RequireVersion(),
+		ConfigVersion: RequireConfigVersion(),
+		Status:        "ok",
+		Host:          RequireHost(),
+	}
+
+	responseJson, err := json.Marshal(result)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(responseJson)
+}
